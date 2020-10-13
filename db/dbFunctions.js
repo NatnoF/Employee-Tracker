60a66b28b9eb07f0196eb81f8dcbb72800b55867
Nicolas Fontan <nico.fontan1@gmail.com>
2020-10-13 17:03:06 -0400

    Finished the View employees by department function

--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -1,5 +1,6 @@
 const index = require("../index");
 const connection = require("./connection");
+const inquirer = require("inquirer");
 
 class DB
 {
@@ -11,17 +12,47 @@ class DB
 
         connection.query(query, function(err, res)
         {
-            console.log("\n");
             console.table(res);
-            console.log("\n");
-        })
 
-        index.loadPrompts();
+            index.loadPrompts();
+        });
     }
 
     viewEmployeesByDepartment()
     {
+        connection.query("SELECT name FROM department", function (err, results)
+        {
+            if (err) throw err;
 
+            inquirer.prompt
+            (
+                {
+                    type: "list",
+                    name: "choice",
+                    message: "What department do you want to search by?",
+                    choices: function()
+                    {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++)
+                        {
+                            choiceArray.push(results[i].name);
+                        }
+                        return choiceArray;
+                    }
+                }
+            )
+            .then(function(answer)
+            {
+                var query = `SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, name AS Department FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id WHERE d.name = "${answer.choice}";`
+                connection.query(query, function(err, res)
+                {
+                    if (err) throw err;
+
+                    console.table(res);
+                    index.loadPrompts();
+                });
+            });
+        });
     }
 
     viewEmployeesByManager()
@@ -36,7 +67,7 @@ class DB
 
     addEmployeeRole()
     {
-        
+
     }
 }
 
