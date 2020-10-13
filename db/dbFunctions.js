0663abd1875ceed59e9e8e84edaad09fb6900ab0
Nicolas Fontan <nico.fontan1@gmail.com>
2020-10-13 15:44:34 -0400

    Finished the viewAllEmployees function

--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -1,10 +1,21 @@
 const index = require("../index");
+const connection = require("./connection");
 
 class DB
 {
     viewAllEmployees()
     {
-        console.log("import works\n");
+        var query = 'SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager '
+        query += 'FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON r.department_id = d.id'
+        
+
+        connection.query(query, function(err, res)
+        {
+            console.log("\n");
+            console.table(res);
+            console.log("\n");
+        })
+
         index.loadPrompts();
     }
 }
